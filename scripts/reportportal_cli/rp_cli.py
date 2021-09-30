9b867be51eb49c6fd41519f6eedb2eb6dff08612
Shveta Sachdeva <sshveta@sshveta.remote.csb>
2021-09-29 20:11:25 -0700

    Fixed error

--- scripts/reportportal_cli/rp_cli.py
+++ scripts/reportportal_cli/rp_cli.py
@@ -271,48 +271,6 @@ class Mtv(Migration):
 # END: Class Mtv
 
 
-class MA(Migration):
-
-    # These properties will be attached as a simple (not key:value pair) tag to each test case
-    properties_to_parse = ['rhv_tier']
-
-    @staticmethod
-    def get_testcase_name(case):
-        """Example: cfme/tests/test_rest.py::test_product_info[rhv_cfme_integration]"""
-        file, classname, name = case.get("@file"), case.get("@classname").split(".")[-1], case.get("@name")
-        # If a test case is encapsulated in pytest class, include the class in test case signature
-        if classname.startswith('Test'):
-            return "{}::{}::{}".format(file, classname, name)
-        else:
-            return "{}::{}".format(file, name)
-
-    @staticmethod
-    def get_testcase_description(case):
-        """Include info on skip reason and time it took to execute."""
-        skip_msg = '\n' + case.get('skipped').get('@message') if case.get('skipped') else ''
-        return "Time: {}{}".format(case.get('@time'), skip_msg)
-
-    def get_tags(self, case, test_owners={}):
-        """Only get values of properties we are explicitly interested in."""
-        if test_owners:
-            raise NotImplementedError('Test owners not implemented for CFME.')
-
-        tags = []
-
-        if 'properties' in case.keys():
-            properties = case.get('properties').get('property')
-            if not isinstance(properties, list):
-                properties = [properties]
-            for p in properties:
-                if p.get("@name") in self.properties_to_parse:
-                    tags.append(p.get("@value"))
-
-        return tags
-
-    def should_create_folders_in_launch(self):
-        return False
-
-
 class RpManager:
     def __init__(self, config, strategy):
         self.url = config.get('rp_endpoint')
@@ -405,7 +363,7 @@ class RpManager:
     def _end_launch(self):
         self.service.finish_launch(end_time=timestamp())
         self.service.terminate()
-        self.launch_id = self.service.rp_client.launch_id
+        self.launch_id = self.service.launch_id
 
     def _upload_attachment(self, file, name):
         with open(file, "rb") as fh:
@@ -478,9 +436,6 @@ class RpManager:
             item_type="SUITE",
         )
 
-    def _close_folder(self):
-        self.service.finish_test_item(item_id=test_item_id, end_time=timestamp(), status=status)
-        # self.service.finish_test_item(end_time=timestamp(), status=None)
 
     def feed_results(self):
         self._start_launch()
@@ -547,7 +502,6 @@ class RpManager:
             else:
                 status = 'PASSED'
             self.service.finish_test_item(item_id=test_item_id, end_time=timestamp(), status=status)
-            # self.service.finish_test_item(end_time=timestamp(), status=status, issue=issue)
 
         if self.strategy.should_create_folders_in_launch():
             self._close_folder()
